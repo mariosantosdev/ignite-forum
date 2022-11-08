27b7ea793b9066d4c9ce46cd09bc0017c607c512
Mário Santos <mariodev7@gmail.com>
2022-11-08 18:23:45 -0300

    populate home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./styles/app.module.css";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Mário Santos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+          />
+          <Post
+            author="Mário Santos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod."
+          />
+        </main>
+      </div>
     </div>
   );
 }
